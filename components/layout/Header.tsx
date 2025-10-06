c172e7b4af1ceb18acfc1a73e2044bc35eff1dfd
Nicolas Vazquez <nicorvazquezs@gmail.com>
2025-10-06 11:47:38 -0300

    Update Header component to adjust shadow effect on scroll, enhancing visual consistency. Change shadow color to improve contrast and maintain design coherence.

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -15,9 +15,7 @@ export default function Header() {
   return (
     <header
       className={`fixed top-5 left-1/2 transform -translate-x-1/2 w-[90%] md:w-3/4 px-4 z-50 bg-transparent backdrop-blur-lg border border-transparent rounded-2xl transition-shadow duration-100 ${
-        isScrolled
-          ? "border-white/10 shadow-[0_3px_20px_rgba(255,255,255,0.1)]"
-          : ""
+        isScrolled ? "border-white/10 shadow-[0_3px_20px_rgba(0,0,0,0.1)]" : ""
       }`}
     >
       <nav>
